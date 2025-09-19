263a67346ba3128ee8bae51a9e035a18dddad91c
катя <katrin.stepanenko.07@mail.ru>
2025-09-20 05:45:20 +0700

    Add files via upload

---
+++ v-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main() 
+{
+    // задание 1
+    {
+        double toplivo;
+        std::cout << "Ввведите объем топлива (в литрах): ";
+        std::cin >> toplivo;
+
+        if (toplivo > 50) {
+            std::cout << "Топлива достаточно" << std::endl;
+        } else if (20 <= toplivo && toplivo <= 50) {
+            std::cout << "Рекомендуется заправка" << std::endl;
+        } else if (toplivo < 50) {
+            std::cout << "Топливо на исходе, срочно заправьтесь" << std::endl;
+        }
+    }
+    //задание 2
+    {
+        double litr;
+        std::cout << "Введите количесво литров: " << std::endl;;
+        while (litr >= 0) {
+            std::cin >> litr;
+            if (litr < 0) {
+                std::cout << "Ошибка" << std::endl;
+                break;
+            }
+            else if (std::cin.fail()) {
+                std::cout << "Ошибка" << std::endl;
+                break;
+            }
+            }
+        }
+}
