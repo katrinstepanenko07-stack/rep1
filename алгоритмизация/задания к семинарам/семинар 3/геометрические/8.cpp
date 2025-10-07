df5ad8b30420ed05a86aa2a11c7557436fa251b6
катя <katrin.stepanenko.07@mail.ru>
2025-10-08 01:57:41 +0300

    Update 8.cpp

---
+++ алгоритмизация/задания к семинарам/семинар 3/геометрические/8.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    int n = 2*2;
+    double massiv[n], arr_x[n/2], arr_y[n/2];
+    int count_x = 0, count_y = 0;
+    std::cout << "Ваши координаты: " << std::endl;
+    for (int i=0; i < n; i++) {
+        std::cin >> massiv[i];
+        if (i % 2 == 0) {
+            arr_x[count_x] = massiv[i];
+            count_x++;
+        } else {
+            arr_y[count_y] = massiv[i];
+            count_y++;
+        }
+    }
+
+    std::cout << "массив для x и y: ";
+    for (int i=0; i<n; i++) {
+        std::cout << massiv[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "массив для x: ";
+    for (int i=0; i<n/2; i++) {
+        std::cout << arr_x[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "массив для y: ";
+    for (int i=0; i<n/2; i++) {
+        std::cout << arr_y[i] << " ";
+    }
+    std::cout << std::endl;
+
+    double d = pow((pow((arr_x[1] - arr_x[0]), 2) + pow((arr_y[1] - arr_y[0]), 2)), 0.5);
+    
+    std::cout << "Длина отрезка: " << d << std::endl;
+}
