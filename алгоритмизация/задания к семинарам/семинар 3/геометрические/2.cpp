689c13b4d76698568e8d9c920ab8b1017ced77a5
катя <katrin.stepanenko.07@mail.ru>
2025-10-07 21:44:19 +0300

    Update 2.cpp

--- алгоритмизация/задания к семинарам/семинар 3/геометрические/2.cpp
+++ алгоритмизация/задания к семинарам/семинар 3/геометрические/2.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
     int n;
     std::cout << "Введите колич вершин: ";
     std::cin >> n;
+    if (n < 3) {
+        std::cout << "Нельзя построить многоугольник";
+        return 0;
+    }
     std::cout << "Введите координаты: " << std::endl;
-    double massiv[n*2], arr_x[n/2], arr_y[n/2];
+    double massiv[n+n], arr_x[n], arr_y[n];
     int count_x = 0, count_y = 0;
-    for (int i=0; i < n*2; i++) {
+    for (int i=0; i < n+n; i++) {
         std::cin >> massiv[i];
         if (i % 2 == 0) {
             arr_x[count_x] = massiv[i];
             count_x++;
-        } else {
+        } else if (i % 2 == 1) {
             arr_y[count_y] = massiv[i];
             count_y++;
         }
@@ -34,4 +39,19 @@ int main() {
         std::cout << arr_y[i] << " ";
     }
     std::cout << std::endl;
+
+    double summa_d = 0;
+    for (int i = 0; i<n-1; i++) {
+        double tek_d = 0;
+        tek_d += pow((pow((arr_x[i+1] - arr_x[i]), 2) + pow((arr_y[i+1] - arr_y[i]), 2)), 0.5);
+        std::cout << i+1 << " сторона) " << tek_d << std::endl;
+        summa_d += tek_d;
+    }
+
+    double ost = 0;
+    ost = pow((pow((arr_x[0] - arr_x[n-1]), 2) + pow((arr_y[0] - arr_y[n-1]), 2)), 0.5);
+    std::cout << n << " сторона) " << ost << std::endl;
+    summa_d += ost;
+
+    std::cout << "Периметр вашего многоугольника: " << summa_d << std::endl;
 }
