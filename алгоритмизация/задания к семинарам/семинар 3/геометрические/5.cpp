d1132d9f9a9b7b352bc898d9828eb4823afb86df
катя <katrin.stepanenko.07@mail.ru>
2025-10-08 00:57:43 +0300

    Update 5.cpp

---
+++ алгоритмизация/задания к семинарам/семинар 3/геометрические/5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+int main() {
+    int n = 3*2;
+    double massiv[n], arr_x[n/2], arr_y[n/2];
+    int count_x = 0, count_y = 0;
+    std::cout << "Ваши координаты: " << std::endl;
+    for (int i=0; i < n; i++) {
+        std::cin >> massiv[i];
+        if (i % 2 == 0) {
+            arr_x[count_x] = massiv[i];
+            count_x++;
+        } else {
+            arr_y[count_y] = massiv[i];
+            count_y++;
+        }
+    }
+
+    std::cout << "массив для x и y: ";
+    for (int i=0; i<n; i++) {
+        std::cout << massiv[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "массив для x: ";
+    for (int i=0; i<n/2; i++) {
+        std::cout << arr_x[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "массив для y: ";
+    for (int i=0; i<n/2; i++) {
+        std::cout << arr_y[i] << " ";
+    }
+    std::cout << std::endl;
+
+    double ab_ac = (arr_x[1] - arr_x[0]) * (arr_y[2] - arr_y[0]) - (arr_y[1] - arr_y[0]) * (arr_x[2] - arr_x[0]);
+    if (ab_ac == 0) {
+        std::cout << "точки лежат на одной прямой!" << std::endl;
+    } else {
+        std::cout << "точки НЕ лежат на одной прямой!" << std::endl;
+    }
+
+}
